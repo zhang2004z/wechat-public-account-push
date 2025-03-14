b357a7f3a21622ac696529c06bbec43723f80252
zhang2004z <111993607+zhang2004z@users.noreply.github.com>
2025-03-14 20:15:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,1 +1,62 @@
-{"APP_ID":"wx14f71db15514e0dc","APP_SECRET":"3b4a91e8dc137834a42db840dcb0207b","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"","CALLBACK_USERS":[{"name":"自己","id":"oyZ096gQ6mVF6-idoHsrb2xKrBC8"}],"USERS":[{"name":"小宝贝","id":"oyZ096o-nLWZ9fFGLpru-5szvIDk","useTemplateId":"e4907I3htMKiAl7VOUunakwNiY7W4dO3AEEXVGwAydw","province":"北京省","city":"北京市朝阳区","horoscopeDate":"11-15","horoscopeDateType":"今日","openUrl":"https://baidu.com/","festivals":[{"type":"生日","name":"美丽可爱的小宝贝","date":"11-15","year":"2004"},{"type":"节日","name":"恋爱纪念日","date":"02-15","year":"2025"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,"earthyLoveWords":true,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}}
+/* eslint-disable */
+
+/**
+ * 此项目配置为方便新人使用，已缩减至最简配置。
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 自行添加属性，以支持更多个性化功能
+ */
+const USER_CONFIG = {
+
+  // 使用微信测试号：公众号APP_ID
+  APP_ID: 'wx14f71db15514e0dc',
+
+  // 使用微信测试号：公众号APP_SECRET
+  APP_SECRET: '3b4a91e8dc137834a42db840dcb0207b',
+
+  PROVINCE: '北京',
+  CITY: '朝阳区',
+
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: '美丽可爱的小宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: '	e4907I3htMKiAl7VOUunakwNiY7W4dO3AEEXVGwAydw',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'e4907I3htMKiAl7VOUunakwNiY7W4dO3AEEXVGwAydw',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '11-15',
+      festivals: [
+        
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '美丽可爱的小宝贝', year: '2004', date: '11-15',
+        },
+        {
+          type: '节日', name: '恋爱纪念日', year: '2025', date: '02-15',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2025-02-15' },
+        
+      ],
+    },
+  ],
+
+
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: '',
+
+  CALLBACK_USERS: [
+    {
+      name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: '',
+    }
+  ],
+
+}
+
+module.exports = USER_CONFIG
