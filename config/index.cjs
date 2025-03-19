3b5e7964b88c7432bb5930bb76f013cc802838d0
一休不是光头 <111993607+zhang2004z@users.noreply.github.com>
2025-03-19 21:11:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,71 +1,21 @@
 /* eslint-disable */
+
+/**
+ * 此项目配置为方便新人使用，已缩减至最简配置。
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 自行添加属性，以支持更多个性化功能
+ */
 const USER_CONFIG = {
-  USE_PASSAGE: 'wechat-test',
-  
+
+  // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx14f71db15514e0dc',
 
-
+  // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '3b4a91e8dc137834a42db840dcb0207b',
-  
 
-  // 功能开关,打开：true，关闭：false
-  SWITCH: {
-    /** 每日天气 */
-    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    weather: true,
+  PROVINCE: '北京市',
+  CITY: '北京市',
 
-    /** 节假日 */
-    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    holidaytts: true,
-
-    /** 每日N句 */
-    // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    CIBA: true,
-    
-    // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    oneTalk: true,
-    
-    // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    earthyLoveWords: true,
-    
-    
-    // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    poisonChickenSoup: true,
-    
-    /** 星座运势 */
-    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    horoscope: true,
-  
-    /** 生日消息和节日消息 */
-    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    birthdayMessage: true,
-  
-  },
-  
-  TIAN_API: {
-    // 天行API KEY，如果使用天行API则需要填写此项
-    key: '274947d733464108f31c3cac9867d1b4',
-    
-    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
-    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    morningGreeting: true,
-  
-  
-  },
-  
-  /** 是否给文字设置多彩颜色, 和emoji不兼容 */
-  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
-  IS_SHOW_COLOR: false,
-  
-  /** 每日一言 */
-  // 每日一言的内容类型
-  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-  LITERARY_PREFERENCE: '网络',
-  
-
-  /**
-   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
-   */
   USERS: [
     {
       // 想要发送的人的名字
@@ -73,145 +23,43 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyZ096kZRNp5k5jj6DozU2krPPOU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'aMTlx-qC16bRVyQC_WkYkPNsgBQQcBrjDgevltWLlsU',
-      // 所在省份或城市，也可以不填
-      province: '北京市',
-      // 所在城市或县区
-      city: '北京市',
-      // 新历生日, 仅用作获取星座运势, 格式必须
+      useTemplateId: 'zKI2wCBMYNXsxXSDPbPTSJ2sXXi9PIlH17TMsEdUdyc',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-15',
-      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: '',
-      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
+        
+       
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '宝宝', year: '2004', date: '11-15',
         },
         {
-          type: '节日', name: '纪念日', year: '2025', date: '02-15',
-        }
+          type: '节日', name: '相识纪念日', year: '202', date: '02-15',
+        },
       ],
-      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2025-02-15' },
+        { keyword: 'birthday_message.DATA', date: '2025-02-15' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-        // 退伍日
-        { keyword: 'ex_day', date: '2022-09-10' }
+        { keyword: 'marry_day', date: '2025-01-15' },
       ],
-
-    
     },
-    
-    // 你可以不断按格式往下增加
-    // ...
   ],
 
-  /**
-   * 【推送完成提醒】 相关，主要用来展示发送是否成功/失败的数据
-   */
 
-  // 使用微信测试号：【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  // 使用其他通道时，请填写 config/template-config.cjs 中【推送完成提醒】模板的id
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: 'y1LDleOcTQtZVqaUjdmkTBGP8CP7bQlK9RrMn-brhnw',
 
-  // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
     {
-      // 一般都填自己
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: 'oyZ096gQ6mVF6-idoHsrb2xKrBC8',
-      useTemplateId: 'y1LDleOcTQtZVqaUjdmkTBGP8CP7bQlK9RrMn-brhnw',
     }
-    // 你可以不断按格式往下增加
-    // ...
   ],
 
-  /**
-   * 信息配置
-   */
-
-  /** 
-   * 天气相关
-   * 
-   * 可以填 省份或者市区， PROVINCE 的区域必须包含 CITY
-   * 例如： PROVINCE：'广东' CITY：'惠州'
-   * 例如： PROVINCE：'咸阳' CITY：'长武'
-   *
-   * 甚至 PROVINCE 可以不填
-   * 例如： PROVINCE：'' CITY：'长武'
-   * */
-  
-  // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: '北京',
-  // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: '北京市',
-
-  /** 重要节日相关 */
-
-  /**
-   * 重要节日，修改名字为对应需要显示的名字, date 仅填月日即可, 请严格按照示例填写
-   * type必须填！ 只能 “生日” 和 “节日” 二选一!
-   *
-   * --- 阴历配置开始 ---
-   * 如果日期使用阴历，请在 “生日” 或 "节日“ 前添加 * 符号
-   * --- 阴历配置结束 ---
-   *
-   * 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
-   * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
-   *
-   * --- 是否展示周岁信息开始 ---
-   * isShowAge: true 展示岁数, 仅type为生日生效
-   * isShowAge: false 不展示岁数, 仅type为生日生效
-   * 删除isShowAge属性，也会不展示岁数
-   * --- 是否展示周岁信息结束 ---
-   */
-  
-  /**
-   * 限制重要节日的展示条目, 需要填写数字;
-   * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
-   * 如果为0, 则默认展示全部
-   */
-  FESTIVALS_LIMIT: 0,
-
-  /** 日期相关 */
-
-  /** 你现在可以随心增加你认为的所有的需要纪念的日子啦！
-   * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
-   * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }}
-   * */
-  CUSTOMIZED_DATE_LIST: [
-    // 在一起的日子
-    { keyword: 'love_day', date: '2025-02-15' },
-    // 结婚纪念日
-    { keyword: 'marry_day', date: '2022-09-09' },
-    // 退伍日
-    { keyword: 'ex_day', date: '2022-09-10' }
-  ],
-
-  /** 插槽 */
-
-  /** 你可以在这里写超多的你想显示的内容了！
-   * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
-   * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }}
-   * */
-  SLOT_LIST: [
-
-    // 这样配置的话，就会每次随机选一句话发送
-    {
-      keyword: 'lover_prattle',
-      contents: [
-        '因为太喜欢你，所以看谁都像是情敌。',
-        '申请成为你爱里的永久居民。',
-        '你很傻，你很笨，可我还是很羡慕你，因为你有我',
-        '遇见你，就好像捡到了100斤的运气'
-      ],
-    }
+}
 
 module.exports = USER_CONFIG
+
